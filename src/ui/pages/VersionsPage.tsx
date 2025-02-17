74252f02ce35266ceff622b217f1d98397dc8217
Nad GarDez <iranad@hotmail.com>
2025-02-16 20:20:01 -0400

    added request to versions page

--- src/ui/pages/VersionsPage.tsx
+++ src/ui/pages/VersionsPage.tsx
@@ -1,5 +1,5 @@
 import { Box, Icon, IconButton, LinearProgress, Paper } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 import { AbstractTable } from "../components/AbstractTable";
 import { type version } from "../../types/versionTypes";
 import { useGetVersions } from "../../hooks/useGetVersions";
@@ -13,6 +13,7 @@ import {
 	startModalForm,
 } from "../../redux/slicers/appSlicer";
 import {
+	activeitemSelector,
 	clearActiveItem,
 	initializeActiveItem,
 } from "../../redux/slicers/activeItemSlicer";
@@ -23,11 +24,56 @@ import {
 	versionsFormSchema,
 	versionsFormStructure,
 } from "../../constants/formConstants";
+import {
+	versionsDeleteRequest,
+	versionsPostRequest,
+	versionsPutRequest,
+} from "../../utils/apiRequest";
+import { useSnackbar } from "notistack";
+import { internalSessionSelector } from "../../redux/slicers/internalSessionSlice";
+import { useLocalRequest } from "../../hooks/useLocalRequest";
 
 export const VersionsPage = withInternalSession((): JSX.Element => {
 	const { data, status, totalCount, reload, limit, page } = useGetVersions();
-	const dispatch = useAppDispatch();
 	const formStatus = useAppSelector(modalFormStatusSelector);
+
+	const dispatch = useAppDispatch();
+	const { enqueueSnackbar } = useSnackbar();
+
+	const { refetch: refetchPut, reducerStatus: statusPut } =
+		useLocalRequest(versionsPutRequest);
+
+	const { refetch: refetchDelete, reducerStatus: statusDelete } =
+		useLocalRequest(versionsDeleteRequest);
+
+	const { refetch: refetchPost, reducerStatus: statusPost } =
+		useLocalRequest(versionsPostRequest);
+
+	const token = useAppSelector(internalSessionSelector);
+
+	const { data: activeItem } = useAppSelector(activeitemSelector);
+
+	useEffect(() => {
+		if (
+			statusPut === "SUCCESSED" ||
+			statusDelete === "SUCCESSED" ||
+			statusPost === "SUCCESSED"
+		) {
+			closeModal();
+			reload({
+				page,
+				limit: 5,
+			});
+			enqueueSnackbar("Success", { variant: "success" });
+		} else if (
+			statusPut === "ERROR" ||
+			statusDelete === "ERROR" ||
+			statusPost === "ERROR"
+		) {
+			closeModal();
+			enqueueSnackbar("Error. Try again", { variant: "error" });
+		}
+	}, [statusPut, statusDelete, statusPost]);
 
 	const onChangePagination = (page: number, rowsPerPage: number): void => {
 		reload({
@@ -36,33 +82,21 @@ export const VersionsPage = withInternalSession((): JSX.Element => {
 		});
 	};
 
-	if (status === "LOADING" || status === "NEUTRAL") {
-		return (
-			<Box sx={{ width: "100%" }}>
-				<LinearProgress color="primary" />
-			</Box>
-		);
-	}
-
 	const onSubmit = (values: version): void => {
-		// if (formStatus === "CREATE") {
-		// 	void refetchPost({
-		// 		token,
-		// 		bodyObject: {
-		// 			...values,
-		// 			type: 1,
-		// 			idEvent: id,
-		// 		},
-		// 		eventId: id,
-		// 	});
-		// } else if (formStatus === "EDIT") {
-		// 	void refetchPut({
-		// 		token,
-		// 		bodyObject: values,
-		// 		bannerId: values.idResource,
-		// 		eventId: id,
-		// 	});
-		// }
+		if (formStatus === "CREATE") {
+			void refetchPost({
+				token,
+				bodyObject: {
+					...values,
+				},
+			});
+		} else if (formStatus === "EDIT") {
+			void refetchPut({
+				token,
+				bodyObject: values,
+				idVersion: values.idVersion,
+			});
+		}
 		console.log(values);
 	};
 
@@ -84,14 +118,26 @@ export const VersionsPage = withInternalSession((): JSX.Element => {
 		openModal("EDIT");
 	};
 
-	const onDelete = (eventId: string): void => {
-		console.log(eventId);
-		// void refetchDelete({
-		// 	token,
-		// 	bannerId,
-		// 	eventId: id,
-		// });
+	const onDelete = (idVersion: string): void => {
+		console.log(idVersion);
+		void refetchDelete({
+			token,
+			idVersion,
+		});
 	};
+
+	const onCreate = (): void => {
+		startActiveItem({}); // should be a default value for void form
+		openModal("CREATE");
+	};
+
+	if (status === "LOADING" || status === "NEUTRAL") {
+		return (
+			<Box sx={{ width: "100%" }}>
+				<LinearProgress color="primary" />
+			</Box>
+		);
+	}
 
 	return (
 		<>
@@ -102,7 +148,7 @@ export const VersionsPage = withInternalSession((): JSX.Element => {
 				<PageToolbar
 					title="Versions"
 					onAdd={() => {
-						console.log("adding");
+						onCreate();
 					}}
 				/>
 				<AbstractTable<version>
@@ -147,7 +193,7 @@ export const VersionsPage = withInternalSession((): JSX.Element => {
 					loading={false}
 					fields={versionsFormStructure}
 					scheme={versionsFormSchema}
-					initialValues={data ?? {}}
+					initialValues={activeItem ?? {}}
 					onSubmit={onSubmit}
 					onDimiss={closeModal}
 				/>
