96c83940ea58a92aa6f64b40304dc392365f94ab
Nad GarDez <iranad@hotmail.com>
2025-02-17 12:15:49 -0400

    deleted from in groups page

--- src/ui/pages/GroupsPage.tsx
+++ src/ui/pages/GroupsPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Icon, IconButton, LinearProgress, Paper } from "@mui/material";
+import { Box, LinearProgress, Paper } from "@mui/material";
 import React from "react";
 import { AbstractTable } from "../components/AbstractTable";
 import type { group } from "../../types/groupTypes";
@@ -6,28 +6,9 @@ import { useGetGroups } from "../../hooks/useGetGroups";
 import { groupsTableStructure } from "../../constants/tableConstants";
 import { withInternalSession } from "../../HOCs/withInternalSession";
 import { PageToolbar } from "../components/PageToolbar";
-import { ModalForm } from "../components/ModalForm";
-import { AbstractForm } from "../components/AbstractForm";
-import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
-import {
-	hideModalForm,
-	modalFormStatusSelector,
-	startModalForm,
-} from "../../redux/slicers/appSlicer";
-import { type modalFormStatus } from "../../types/uiTypes";
-import {
-	clearActiveItem,
-	initializeActiveItem,
-} from "../../redux/slicers/activeItemSlicer";
-import {
-	groupsFormSchema,
-	groupsFormStructure,
-} from "../../constants/formConstants";
 
 export const GroupsPage = withInternalSession((): JSX.Element => {
 	const { data, status, totalCount, reload, limit, page } = useGetGroups();
-	const formStatus = useAppSelector(modalFormStatusSelector);
-	const dispatch = useAppDispatch();
 
 	const onChangePagination = (page: number, rowsPerPage: number): void => {
 		reload({
@@ -43,55 +24,6 @@ export const GroupsPage = withInternalSession((): JSX.Element => {
 			</Box>
 		);
 	}
-
-	const onSubmit = (values: group): void => {
-		// if (formStatus === "CREATE") {
-		// 	void refetchPost({
-		// 		token,
-		// 		bodyObject: {
-		// 			...values,
-		// 			type: 1,
-		// 			idEvent: id,
-		// 		},
-		// 		eventId: id,
-		// 	});
-		// } else if (formStatus === "EDIT") {
-		// 	void refetchPut({
-		// 		token,
-		// 		bodyObject: values,
-		// 		bannerId: values.idResource,
-		// 		eventId: id,
-		// 	});
-		// }
-		console.log(values);
-	};
-
-	const startActiveItem = (item: object): void => {
-		dispatch(initializeActiveItem(item));
-	};
-
-	const openModal = (mode: modalFormStatus): void => {
-		dispatch(startModalForm(mode));
-	};
-
-	const closeModal = (): void => {
-		dispatch(hideModalForm());
-		dispatch(clearActiveItem());
-	};
-
-	const onEdit = (item: group): void => {
-		startActiveItem(item as object);
-		openModal("EDIT");
-	};
-
-	const onDelete = (eventId: string): void => {
-		console.log(eventId);
-		// void refetchDelete({
-		// 	token,
-		// 	bannerId,
-		// 	eventId: id,
-		// });
-	};
 
 	return (
 		<>
@@ -112,45 +44,8 @@ export const GroupsPage = withInternalSession((): JSX.Element => {
 					page={page}
 					total={totalCount}
 					onChangePagination={onChangePagination}
-					renderActions={item => (
-						<>
-							<IconButton
-								onClick={() => {
-									onEdit(item);
-								}}
-							>
-								<Icon color="primary" fontSize="inherit">
-									edit
-								</Icon>
-							</IconButton>
-							<IconButton
-								onClick={() => {
-									onDelete(item.oktaId);
-								}}
-							>
-								<Icon color="error" fontSize="inherit">
-									delete
-								</Icon>
-							</IconButton>
-						</>
-					)}
 				/>
 			</Paper>
-
-			<ModalForm
-				open={formStatus !== "HIDDEN"}
-				title={"Banners"}
-				handleClose={closeModal}
-			>
-				<AbstractForm
-					loading={false}
-					fields={groupsFormStructure}
-					scheme={groupsFormSchema}
-					initialValues={data ?? {}}
-					onSubmit={onSubmit}
-					onDimiss={closeModal}
-				/>
-			</ModalForm>
 		</>
 	);
 });
